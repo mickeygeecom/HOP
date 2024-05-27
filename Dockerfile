1fe2bf79f1a4e368c1687b15bbccb2cb9419f954
gEE <mail@mickeygee.com>
2024-05-27 16:38:43 +0200

    Add GitHub Actions workflow for Fly.io deployment

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,28 @@ FROM golang:${GO_VERSION}-bookworm as builder
 WORKDIR /usr/src/app
 COPY go.mod go.sum ./
 RUN go mod download && go mod verify
-COPY . .
-RUN go build -v -o /run-app .
 
+COPY . .
+
+# Specify the directory where main.go is located
+WORKDIR /usr/src/app/cmd/server
+
+# Build the Go application
+RUN go build -v -o /run-app .
 
 FROM debian:bookworm
 
+# Copy the compiled binary to the final image
 COPY --from=builder /run-app /usr/local/bin/
+
+# Copy the web directory to the final image
+COPY --from=builder /usr/src/app/web /usr/src/app/web
+
+# Set the working directory to the location of the web directory
+WORKDIR /usr/src/app
+
+# Expose ports
+EXPOSE 80
+EXPOSE 443
+
 CMD ["run-app"]
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,32 +17,31 @@ func main() {
 	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
-	// Get environment variables
 	httpPort := os.Getenv("HTTP_PORT")
-	httpsPort := os.Getenv("HTTPS_PORT")
+	// httpsPort := os.Getenv("HTTPS_PORT")
 
 	db.InitDB()
 	router := api.SetupRouter()
 
 	// Start HTTP server in a Go routine so that it runs concurrently
-	go func() {
-		log.Println("Starting HTTP server on port:", httpPort)
-		if err := http.ListenAndServe(":"+httpPort, http.HandlerFunc(redirectTLS)); err != nil {
-			log.Fatalf("Failed to start HTTP server on port %s: %v", httpPort, err)
-		}
-	}()
+	// go func() {
+	log.Println("Starting HTTP server on port:", httpPort)
+	if err := http.ListenAndServe(":"+httpPort, router); err != nil {
+		log.Fatalf("Failed to start HTTP server on port %s: %v", httpPort, err)
+	}
+	// }()
 
 	// Start HTTPS server
-	log.Println("Starting HTTPS server on port:", httpsPort)
-	if err := http.ListenAndServeTLS(":"+httpsPort, "./web/ssl/fullchain.pem", "./web/ssl/privkey.pem", router); err != nil {
-		log.Fatalf("Failed to start HTTPS server on port %s: %v", httpsPort, err)
-	}
+	// log.Println("Starting HTTPS server on port:", httpsPort)
+	// if err := http.ListenAndServeTLS(":"+httpsPort, "./web/ssl/fullchain.pem", "./web/ssl/privkey.pem", router); err != nil {
+	// 	log.Fatalf("Failed to start HTTPS server on port %s: %v", httpsPort, err)
+	// }
 }
 
 // redirectTLS redirects HTTP requests to HTTPS
-func redirectTLS(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
-}
+// func redirectTLS(w http.ResponseWriter, r *http.Request) {
+// 	http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
+// }
